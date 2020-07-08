917e23d6939647066486da2d25dc36b5e2a10448
mark-zarandi <53958026+mark-zarandi@users.noreply.github.com>
2020-07-08 18:22:51 -0400

    Delete get_first.py

--- get_first.py
+++
@@ -1,9 +0,0 @@
-import feedparser
-
-def get_recent(rss_address):
-	d = feedparser.parse(rss_address)
-	return d.entries[0].enclosures[0].href
-
-if __name__ == "__main__":
-	podcast_address = 'https://www.npr.org/rss/podcast.php?id=500005'
-	print(get_recent(podcast_address))
